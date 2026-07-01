44fa3ca0320960543e9eb3ab031cb08a54363b1b
PRIYANSHUKUMAWAT1906 <er.priyanshu1906@gmail.com>
2026-07-01 10:58:48 +0530

    ready for deploy

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,10 +1,11 @@
 const { Pool}=require("pg");
 require("dotenv").config();
-const pool=new Pool({
-    user:"postgres",
-    host:"localhost",
-    database:"ecommerce",
-    password:process.env.DB_PASSWORD,
-    port:5432
-})
-module.exports= pool;
+const pool = new Pool({
+    connectionString:
+        process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+module.exports = pool;
