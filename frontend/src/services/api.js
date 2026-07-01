a81845c6b779bd871bea1cbdbf40ac5d2d5de986
PRIYANSHUKUMAWAT1906 <er.priyanshu1906@gmail.com>
2026-07-01 18:09:59 +0530

    fix server issue

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL:
-"https://ai-e-commerce-recommendation-agent.onrender.com"
+"https://ai-e-commerce-recommendation-agent.onrender.com/api"
 });
 
 export default api;
